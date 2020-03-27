31a4eba93ae894253ad665859dde58d1b5f032cf
Takashi Shinohara <takashi.shinohara@outlook.com>
2020-03-27 18:05:34 +0900

    1.2.1 release

--- source/src/webparts/calendar/components/CalendarEvent.tsx
+++ source/src/webparts/calendar/components/CalendarEvent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styles from './Calendar.module.scss';
-import { Link, TeachingBubble } from 'office-ui-fabric-react';
+import { Icon, Link, TeachingBubble } from 'office-ui-fabric-react';
 
 import * as strings from 'CalendarWebPartStrings';
 
@@ -42,14 +42,30 @@ export class CalendarEvent extends React.Component<ICalendarEventProps, ICalenda
         {
           this.state.toggle
             ? <TeachingBubble target={this.link} onDismiss={() => this.setState({ toggle: false })}>
-                <div>{this.props.event.title}</div>
-                {this.props.event.allDayEvent ? <div><small>{strings.AllDaysLabel}</small></div> : null }
-                <div><small>
-                {
-                  new DateTime(this.props.event.beginDate).format(strings.DateTimeFormat) + ' - ' + 
-                  new DateTime(this.props.event.endDate).format(strings.DateTimeFormat)
-                }
-                </small></div>
+                <div className={styles.callout}>
+                  <div>
+                    {this.props.event.title}
+                    {
+                      this.props.event.recurrence
+                        ? <Icon className={styles.icon} iconName="Sync"></Icon>
+                        : null
+                    }
+                  </div>
+                  <div className={styles.location}>{this.props.event.location}</div>
+                  <div className={styles.alldays}>
+                  {
+                    this.props.event.allDayEvent
+                      ? strings.AllDaysLabel
+                      : null
+                  }
+                  </div>
+                  <div className={styles.date}>
+                  {
+                    new DateTime(this.props.event.beginDate).format(strings.DateTimeFormat) + ' - ' + 
+                    new DateTime(this.props.event.endDate).format(strings.DateTimeFormat)
+                  }
+                  </div>
+                </div>
               </TeachingBubble>
             : null
         }
