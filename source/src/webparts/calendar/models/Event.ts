04f157c41e0a1cf960b3f58b4b2576e6a2bd1777
Takashi Shinohara <takashi.shinohara@outlook.com>
2020-03-27 18:05:34 +0900

    1.2.1 release

--- source/src/webparts/calendar/models/Event.ts
+++ source/src/webparts/calendar/models/Event.ts
@@ -4,9 +4,11 @@ export class Event {
 
   public id: number;
   public title: string;
+  public location: string;
   public beginDate: Date;
   public endDate: Date;
   public allDayEvent: boolean;
+  public recurrence: boolean;
 
   constructor(item: any, date?: Date) {
     let beginDate = new Date(Date.parse(item.EventDate));
@@ -18,15 +20,19 @@ export class Event {
     if (date == undefined) {
       this.id = item.Id;
       this.title = item.Title;
+      this.location = item.Location;
       this.beginDate = beginDate;
       this.endDate = endDate;
       this.allDayEvent = item.fAllDayEvent;
+      this.recurrence = false;
     } else {
       this.id = item.Id;
       this.title = item.Title;
+      this.location = item.Location;
       this.beginDate = new DateTime(date).setTime(beginDate).toDate();
       this.endDate = new DateTime(date).setTime(endDate).toDate();
       this.allDayEvent = item.fAllDayEvent;
+      this.recurrence = true;
     }
   }
 
